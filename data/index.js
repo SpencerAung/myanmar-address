d6528b8cc5d24278dec68c6aef0cb5a8cafb22a6
SpencerAung <10285698+SpencerAung@users.noreply.github.com>
2019-08-13 12:02:36 +0900

    Add region and states

--- data/index.js
+++ data/index.js
@@ -1,9 +1,79 @@
+const { region, state, unionTerritory } = require('./types');
+
 module.exports = [
   {
-    name: {
-      en: 'Yangon',
-      mm: 'ရန်ကုန်'
-    },
-    ​shortName: 'YG'
-  }
-]
+    name: 'Ayeyarwady',
+    localName: 'ဧရာဝတီ',
+    type: region,
+  },
+  {
+    name: 'Bago',
+    localName: 'ပဲခူး',
+    type: region,
+  },
+  {
+    name: 'Chin',
+    localName: 'ချင်း',
+    type: state,
+  },
+  {
+    name: 'Kachin',
+    localName: 'ကချင်',
+    type: state,
+  },
+  {
+    name: 'Kayah',
+    localName: 'ကယား',
+    type: state,
+  },
+  {
+    name: 'Kayin',
+    localName: 'ကရင်',
+    type: state,
+  },
+  {
+    name: 'Magway',
+    localName: 'မကွေး',
+    type: region,
+  },
+  {
+    name: 'Mandalay',
+    localName: 'မန္တလေး',
+    type: region,
+  },
+  {
+    name: 'Mon',
+    localName: 'မွန်',
+    type: state,
+  },
+  {
+    name: 'Rakhine',
+    localName: 'ရခိုင်',
+    type: state,
+  },
+  {
+    name: 'Shan',
+    localName: 'ရှမ်း',
+    type: state,
+  },
+  {
+    name: 'Sagaing',
+    localName: 'စစ်ကိုင်း',
+    type: region,
+  },
+  {
+    name: 'Tanintharyi',
+    localName: 'တနင်္သာရီ',
+    type: region,
+  },
+  {
+    name: 'Yangon',
+    localName: 'ရန်ကုန်',
+    type: region,
+  },
+  {
+    name: 'Naypidaw',
+    localName: 'နေပြည်တော်',
+    type: unionTerritory,
+  },
+];
---
+++ data/types.js
@@ -0,0 +1,20 @@
+const region = {
+  name: 'region',
+  localName: 'တိုင်းဒေသကြီး',
+};
+
+const state = {
+  name: 'state',
+  localName: 'ပြည်နယ်',
+};
+
+const unionTerritory = {
+  name: 'union territory',
+  localName: 'ပြည်ထောင်စု နယ်မြေ',
+};
+
+module.exports = {
+  region,
+  state,
+  unionTerritory,
+};
